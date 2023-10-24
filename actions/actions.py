61f739de07b68d827d168cd6e01befc47916cd43
juanco1424 <jdcamargouseche@gmail.com>
2023-10-24 17:05:31 -0500

    actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,39 @@
+# from typing import Text, Dict, Any, List
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.events import SlotSet
+# from rasa_sdk.executor import CollectingDispatcher
+
+
+# class ActionGuardarNombre(Action):
+#     def name(self):
+#         return "action_guardar_nombre"
+
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         # Obtener el valor de la entidad "nombre"
+#         nombre = tracker.get_slot("nombre")
+
+#         # Guardar el nombre en una ranura (slot) para usarlo más adelante
+#         return [SlotSet("nombre", nombre)]
+
+# class ActionSaludoPersonalizado(Action):
+#     def name(self):
+#         return "utter_saludo_personalizado"
+
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         # Obtener el nombre desde la ranura (slot)
+#         nombre = tracker.get_slot("nombre")
+
+#         if nombre:
+#             mensaje = f"Hola {nombre}, ¿cómo estás?"
+#         else:
+#             mensaje = "Hola, ¿cómo te llamas?"
+
+#         # Enviar la respuesta al usuario
+#         dispatcher.utter_message(text=mensaje)
+
+#         return []
+
 # This files contains your custom actions which can be used to run
 # custom Python code.
 #
@@ -7,21 +43,90 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),
+            tracker.get_slot("subject"),
+            tracker.get_slot("message")
+        )
+        dispatcher.utter_message("Thanks for providing the details. We have sent you a mail at {}".format(tracker.get_slot("email")))
+        return []
+
+def SendEmail(toaddr,subject,message):
+    fromaddr = "jdcamargouseche@gmail.com"
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = subject
+
+    # string to store the body of the mail
+    body = message
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    # filename = "/home/ashish/Downloads/webinar_rasa2_0.png"
+    # attachment = open(filename, "rb")
+    #
+    # # instance of MIMEBase and named as p
+    # p = MIMEBase('application', 'octet-stream')
+    #
+    # # To change the payload into encoded form
+    # p.set_payload((attachment).read())
+    #
+    # # encode into base64
+    # encoders.encode_base64(p)
+    #
+    # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    #
+    # # attach the instance 'p' to instance 'msg'
+    # msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+
+    # Authentication
+    try:
+        s.login(fromaddr, "SachaCopito2060*")
+
+        # Converts the Multipart msg into a string
+        text = msg.as_string()
+
+        # sending the mail
+        s.sendmail(fromaddr, toaddr, text)
+    except:
+        print("An Error occured while sending email.")
+    finally:
+        # terminating the session
+        s.quit()
